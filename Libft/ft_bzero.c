b683d92ab6405630c6478ad3f02efb0d38834509
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-23 11:32:56 +0100

    Update ft_bzero.c

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:50:09 by abnsila           #+#    #+#             */
-/*   Updated: 2024/10/23 10:31:24 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/10/23 10:52:25 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//TODO[ ] We should compile all files ,before use the ft_memset without recoded here 
+//TODO[ ] We should compile all files ,before use the ft_memset without recoded here
 
 static void	*_ft_memset(void *ptr, int x, size_t n)
 {
