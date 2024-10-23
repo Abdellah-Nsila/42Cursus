ecdaabd9fdb7c627088437bd02341560871d742b
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-23 10:34:42 +0100

    Fix some logic

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -6,13 +6,13 @@
 /*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:44:51 by abnsila           #+#    #+#             */
-/*   Updated: 2024/10/22 11:50:53 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/10/23 09:25:31 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static int	ft_isalpha(int c)
 {
-	return ((c >= 65 && c < 90) || (c >= 97 && c < 122));
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
 }
 
 static int	ft_isdigit(int n)
