2a5610022987c3ba381c4f5ba2ed5577704c8961
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-23 11:33:08 +0100

    Update ft_strchr.c

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,11 +6,25 @@
 /*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:27:05 by abnsila           #+#    #+#             */
-/*   Updated: 2024/10/23 09:30:15 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/10/23 11:30:41 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strchr(const char *str, int search_str)
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
 {
-    
+	unsigned char search_str = c;
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == search_str)
+			return ((char *)(&str[i]));
+		i++;
+	}
+	if (search_str == '\0')
+		return ((char *)(&str[i]));
+	return (NULL);
 }
