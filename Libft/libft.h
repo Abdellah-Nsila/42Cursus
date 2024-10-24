5c99af82369457a97dae359029a7994458ed5127
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-24 10:23:42 +0100

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:12:03 by abnsila           #+#    #+#             */
-/*   Updated: 2024/10/23 11:35:58 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/10/24 08:28:38 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ int	ft_isprint(int c);
 int	ft_strlen(char *str);
 void	*ft_memset(void *ptr, int x, size_t n);
 void	ft_bzero(void *s, size_t n);
+void 	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 int ft_toupper(int c);
 int ft_tolower(int c);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr( const char * str, int c);
-
 
 #endif
