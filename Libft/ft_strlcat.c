b01d292fece40e4de8e64480911722ced022069c
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-24 15:59:26 +0100

    Add ft_strlcat.c

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 15:11:03 by abnsila           #+#    #+#             */
+/*   Updated: 2024/10/24 15:58:21 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+
+	i = 0;	
+	src_len = strlen(src);
+
+	if (dstsize == 0)
+		return (src_len);
+	dst_len = ft_strlen(dst);
+
+	if (dst_len >= dstsize)
+		return (dstsize + src_len);
+		
+	while((dst_len + i) < dstsize - 1 && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
