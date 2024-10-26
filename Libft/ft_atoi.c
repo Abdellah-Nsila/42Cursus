1f6afd50ef330a2e6f0a07ef239fc963229e106b
Abdellah-Nsila <dawndawn074@gmail.com>
2024-10-26 10:09:04 +0100

    Some update

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 09:33:43 by abnsila           #+#    #+#             */
+/*   Updated: 2024/10/26 10:08:33 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	num;
+	int	sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && ft_isdigit((int)str[i]))
+	{
+		num = (num * 10) + (str[i] - 48);
+		i++;
+	}
+	return (num * sign);
+}
