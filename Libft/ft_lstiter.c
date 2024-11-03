4556d627993f261a847c871cffe189d3323cee20
Abdellah-Nsila <dawndawn074@gmail.com>
2024-11-03 11:20:47 +0100

    Some Update

--- Libft/ft_lstiter.c
+++ Libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 21:42:27 by abnsila           #+#    #+#             */
-/*   Updated: 2024/11/03 10:42:06 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/11/03 11:20:27 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,54 +22,3 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
-
-// ---------------------------------------- Work Fine from Normal string To UpperCase string ----------------------------------------
-
-// void	strtoupper(void *str)
-// {
-// 	char	*s;
-
-// 	s = str;
-// 	while (*s)
-// 	{
-// 		*s = ft_toupper(*s);
-// 		s++;
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	t_list	*head;
-// 	t_list	*node1;
-// 	t_list	*node2;
-// 	t_list	*node3;
-// 	t_list	*current;
-
-// 	head = NULL;
-// 	node1 = ft_lstnew("First Node");
-// 	node2 = ft_lstnew("Second Node");
-// 	node3 = ft_lstnew("Third Node");
-// 	ft_lstadd_back(&head, node1);
-// 	ft_lstadd_back(&head, node2);
-// 	ft_lstadd_back(&head, node3);
-	
-// 	printf("Before appling the function 'f':\n");
-// 	current = head;
-// 	while (current)
-// 	{
-// 		printf("%s -> ", (char *)current->content);
-// 		current = current->next;
-// 	}
-// 	printf("NULL\n");
-// 	ft_lstiter(head, strtoupper);
-	
-// 	printf("\nAfter appling the function 'f':\n");
-// 	current = head;
-// 	while (current)
-// 	{
-// 		printf("%s -> ", (char *)current->content);
-// 		current = current->next;
-// 	}
-// 	printf("NULL\n");
-// 	return (0);
-// }
