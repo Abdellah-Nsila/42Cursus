f6a6240ada3224bf60f972942f69cbf799e305e2
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-07 15:25:40 +0100

    Some Update

--- Milestone 1/ft_printf/ft_print_ptr.c
+++ Milestone 1/ft_printf/ft_print_ptr.c
@@ -6,16 +6,16 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:27:36 by abnsila           #+#    #+#             */
-/*   Updated: 2024/12/07 14:49:10 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/12/07 15:25:16 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_print_ptr(void *p)
+int	ft_print_ptr(void *p)
 {
-    if ((void *)p == NULL)
-        return (ft_print_str("(nil)"));
-    return (ft_print_str("0x")
-        + ft_print_hexa((unsigned long int)p, "0123456789abcdef"));
+	if ((void *)p == NULL)
+		return (ft_print_str("(nil)"));
+	return (ft_print_str("0x")
+		+ ft_print_hexa((unsigned long int)p, "0123456789abcdef"));
 }
