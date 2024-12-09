55ebfe829a7a3c05e367be0fb88166f33d0f4dff
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-09 16:45:25 +0100

    Some Update

--- Milestone 1/get_next_line/get_next_line.c
+++ Milestone 1/get_next_line/get_next_line.c
@@ -2,6 +2,16 @@
 
 char	*get_next_line(int fd)
 {
-    
+    static char*line;
+
+
 }
 
+int main()
+{
+    int fd;
+
+    fd = open("test.txt", O_RDONLY);
+    if (fd == -1)
+        printf("Error...");
+}
--- Milestone 1/get_next_line/get_next_line.h
+++ Milestone 1/get_next_line/get_next_line.h
@@ -2,7 +2,8 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
-# include <stdarg.h>
+# include <fcntl.h>
+
 
 char	*get_next_line(int fd);
 
