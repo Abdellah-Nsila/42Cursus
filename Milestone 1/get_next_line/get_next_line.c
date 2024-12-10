9d6af3f6b7fb7d70087f50b5ad3ddeb77c03e473
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-10 15:11:02 +0100

    Some Update

--- Milestone 1/get_next_line/get_next_line.c
+++ Milestone 1/get_next_line/get_next_line.c
@@ -1,33 +1,157 @@
 #include "get_next_line.h"
 
+size_t	ft_strlen(char	*s)
+{
+	int	len;
+
+	len = 0;
+	while (s && s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	unsigned char	search_str;
+	size_t			i;
+
+	search_str = (unsigned char)c;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == search_str)
+			return ((char *)(&str[i]));
+		i++;
+	}
+	if (search_str == '\0')
+		return ((char *)(&str[i]));
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	i = 0;
+	src_len = ft_strlen((char *)src);
+	if (dstsize == 0)
+		return (src_len);
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+
+	i = 0;
+	src_len = ft_strlen((char *)src);
+	if (!dst && !src && dstsize == 0)
+		return (src_len);
+	dst_len = ft_strlen(dst);
+	if (dst_len >= dstsize)
+		return (dstsize + src_len);
+	while (((dst_len + i) < (dstsize - 1)) && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
+
+char	*ft_extractLine(char *s)
+{
+	char	*endline;
+	char	*line;
+	int		len;
+
+	endline = ft_strchr(s, '\n');
+	printf("end line addr: %s\n", endline);
+	len = 0;
+	if (endline)
+	{
+		while (s && s[len] != '\n')
+			len++;
+		line = (char *) malloc((len + 1) * sizeof(char));
+		if (!line)
+			return (NULL);
+		len = 0;
+		while (s && s[len] != '\n')
+		{
+			line[len] = s[len];
+			len++;
+		}
+		line[len] = '\n';
+		return (line);
+	}
+	return ("\n");
+}
+
+char	*ft_handleNextBuf(char *s, char buf[BUFFER_SIZE])
+{
+	char	*temp;
+
+	temp = (char *) malloc((ft_strlen(s) + ft_strlen(buf)) * sizeof(char));
+	if (!temp)
+		return (NULL);
+	if (s)
+		ft_strlcat(temp, s, ft_strlen(s));
+	ft_strlcat(temp, buf, ft_strlen(buf));
+	s = temp;
+	return (s);
+}
+
+static char	*ft_appendBuff(char *static_var, char *buf, ssize_t rb)
+{
+	char	*new_static_var;
+	size_t	len;
+
+    if (!static_var)
+        len = 0;
+    else
+        len = ft_strlen(static_var);
+	new_static_var = (char *) malloc((len + rb + 1) * sizeof(char));
+	if (!new_static_var)
+		return (NULL);
+	if (static_var)
+        ft_strlcpy(new_static_var, static_var, len);
+    ft_strlcat(new_static_var, buf, rb);
+
+    free(static_var);
+    return (new_static_var);
+
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*s;
-	ssize_t		r;
+	static char	*static_var;
+	ssize_t		rb;
 	int			i;
 	char 		buf[BUFFER_SIZE];
+	int		line_len;
+	char	*line;
 
-	while (1)
+	while (rb = read(fd, buf, BUFFER_SIZE) > 0)
 	{
-		r = read(fd, buf, BUFFER_SIZE);
-		if (r > 0)
+		buf[rb] = '\0';
+		static_var = ft_appendBuff(static_var, buf, rb);
+		line = ft_strchr(static_var, '\n');
+		if (line)
 		{
-			// You must read the buff count stored until /n, 
-			// return the line it save the remaining ...
-			
+			line_len = static_var - line + 1;
+			line = strndup(static_var, line_len);
 		}
-	}
-	r = read(fd, buf, BUFFER_SIZE);
-	if (r > 0)
-		while (buf[i])
-		{
-			if (buf[i] == '\n')
 
-			write(1, &buf[i], 1);
-			i++;
-		}
-	else
-		return (NULL);
+	}
 	return (NULL);
 }
 
@@ -38,7 +162,19 @@ int main()
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
-		printf("Error...");
+		printf("Error...\n");
 	
-	get_next_line(fd);
+	// First Call
+	line = get_next_line(fd);
+	if (line)
+		printf("line: %s\n", line);
+	else
+		printf("Error in get next line...\n");
+
+	// Second Call
+	line = get_next_line(fd);
+	if (line)
+		printf("line: %s\n", line);
+	else
+		printf("Error in get next line...\n");
 }
--- Milestone 1/get_next_line/get_next_line.h
+++ Milestone 1/get_next_line/get_next_line.h
@@ -4,6 +4,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include <stdlib.h>
 
 # define BUFFER_SIZE 10
 
