0f4e844c39b6a4cc6553f8b086fb44ee3b79125a
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-11 09:39:39 +0100

    Some Update

--- Milestone 1/get_next_line/get_next_line.h
+++ Milestone 1/get_next_line/get_next_line.h
@@ -11,4 +11,11 @@
 
 char	*get_next_line(int fd);
 
+size_t	ft_strlen(char	*s);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+
+
 #endif
