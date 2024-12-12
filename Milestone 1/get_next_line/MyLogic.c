0091e77f3dbfaf69775fc48dd4d798f9377d09ec
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-12 12:42:53 +0100

    Some Update

--- Milestone 1/get_next_line/MyLogic.c
+++ Milestone 1/get_next_line/MyLogic.c
@@ -6,13 +6,13 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 11:39:38 by abnsila           #+#    #+#             */
-/*   Updated: 2024/12/12 11:53:15 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/12/12 12:40:55 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_free_static_var(char **static_var)
+char	*ft_clean(char **static_var)
 {
 	free(*static_var);
 	*static_var = NULL;
@@ -30,17 +30,20 @@ char	*ft_append_buff(char **static_var, char *buff, ssize_t rb)
 		len = 0;
 	new_static_var = (char *)malloc((len + rb + 1) * sizeof(char));
 	if (!new_static_var)
-		return (ft_free_static_var(static_var));
+	{	
+		free(buff);
+		return (ft_clean(static_var));
+	}
 	if (static_var)
 	{
 		ft_strlcpy(new_static_var, *static_var, len + 1);
-		ft_free_static_var(static_var);
+		ft_clean(static_var);
 	}
 	ft_strlcat(new_static_var, buff, len + rb + 1);
 	return (new_static_var);
 }
 
-char	*ft_get_remaining(char **static_var, char *end_line)
+char	*ft_get_remaining(char **static_var, char *buff, char *end_line)
 {
 	char	*new_static_var;
 	size_t	remaining_len;
@@ -48,9 +51,9 @@ char	*ft_get_remaining(char **static_var, char *end_line)
 	remaining_len = ft_strlen(end_line + 1);
 	new_static_var = (char *)malloc((remaining_len + 1) * sizeof(char));
 	if (!new_static_var)
-		return (ft_free_static_var(static_var));
+		return (ft_clean(static_var));
 	ft_strlcpy(new_static_var, end_line + 1, remaining_len + 1);
-	ft_free_static_var(static_var);
+	ft_clean(static_var);
 	return (new_static_var);
 }
 
@@ -69,58 +72,73 @@ char	*ft_get_line(char *static_var, char *end_line)
 	return (line);
 }
 
-char	*ft_process_line(char **static_var, char *end_line)
+char	*ft_process_read_content(char **static_var, char *buff, char *end_line)
 {
 	char	*line;
 
 	line = ft_get_line(*static_var, end_line);
-	*static_var = ft_get_remaining(static_var, end_line);
+	*static_var = ft_get_remaining(static_var, buff, end_line);
 	if (!line)
-		return (ft_free_static_var(static_var));
+	{
+		free(buff);	
+		return (ft_clean(static_var));
+	}
 	return (line);
 }
 
-char	*ft_handle_end_of_read(char **static_var, ssize_t rb)
+char *ft_handle_end_of_read(char **static_var, ssize_t rb)
 {
-	char	*end_line;
-	char	*line;
+	char *end_line;
+	char *line;
 
-	if (static_var && *static_var
-		&& ft_strchr(*static_var, '\n'))
-			return (ft_process_line(static_var, end_line));
-	if (static_var && *static_var)
+	if (!static_var || !*static_var)
+		return (ft_clean(static_var));
+	end_line = ft_strchr(*static_var, '\n');
+	if (end_line)
+		return (ft_process_read_content(static_var, NULL, end_line));
+	if (rb == 0 && *static_var && **static_var)
 	{
 		line = ft_strdup(*static_var);
-		ft_free_static_var(static_var);
+		ft_clean(static_var);
 		return (line);
 	}
-	if (rb < 0)
-		return (ft_free_static_var(static_var));
-	return (ft_free_static_var(static_var));
+	return (ft_clean(static_var));
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*static_var;
 	ssize_t		rb;
-	char		buff[BUFFER_SIZE + 1];
+	char		*buff;
 	char		*end_line;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
 	while ((rb = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		buff[rb] = '\0';
 		static_var = ft_append_buff(&static_var, buff, rb);
-		end_line = ft_strchr(static_var, '\n');
 		if (!static_var)
+		{
+			free(buff);
 			return (NULL);
+		}
+		end_line = ft_strchr(static_var, '\n');
 		if (end_line)
-			return (ft_process_line(&static_var, end_line));
+		{
+			line = ft_process_read_content(&static_var, buff, end_line);
+			free(buff);
+			return (line);
+		}
 	}
-	ft_handle_end_of_read(&static_var, rb);
+	free(buff);
+	return (ft_handle_end_of_read(&static_var, rb));
 }
+
 
 // int main()
 // {
--- Milestone 1/get_next_line/get_next_line.h
+++ Milestone 1/get_next_line/get_next_line.h
@@ -7,7 +7,7 @@
 # include <stdlib.h>
 # include <string.h>
 
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 999999999
 
 char	*get_next_line(int fd);
 
@@ -16,5 +16,12 @@ char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strdup(const char *s1);
+
+char	*ft_clean(char **static_var);
+char	*ft_append_buff(char **static_var, char *buff, ssize_t rb);
+char	*ft_get_remaining(char **static_var, char *buff, char *end_line);
+char	*ft_get_line(char *static_var, char *end_line);
+char	*ft_process_read_content(char **static_var, char *buff, char *end_line);
+char	*ft_handle_end_of_read(char **static_var, ssize_t rb);
 
 #endif
