367a9d19bb77c4be255502481b0491a1358b451e
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-15 17:57:21 +0100

    Some Update

---
+++ Milestone 2/push_swap/complexity.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	ft_insertion_sort(int *arr, int arr_size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < arr_size)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+}
+
+int main()
+{
+	int arr[] = {4,9,2,0,3,6};
+	int arr_size = sizeof(arr) / sizeof(int);
+
+	printf("||---------------------------- ft_insertion_sort ----------------------------||\n\n");
+	ft_insertion_sort(arr, arr_size);
+	printf("\n||---------------------------------------------------------------------------||\n");
+}
