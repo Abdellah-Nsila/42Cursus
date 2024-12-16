77e1d0c04d51152453345777770785f687d750fe
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-16 18:32:23 +0100

    Some Update

--- Milestone 2/push_swap/complexity.c
+++ Milestone 2/push_swap/complexity.c
@@ -12,21 +12,44 @@ void	ft_swap(int *a, int *b)
 
 int	ft_insertion_sort(int *arr, int arr_size)
 {
+	
 	size_t	i;
 	size_t	j;
 	int		temp;
 
-	i = 0;
+	i = 1;
 	while (i < arr_size)
 	{
-		printf("%d\n", arr[i]);
-		while (i < arr_size)
+		j = i;
+		printf("%d\n", j);
+		while (j >= 0)
 		{
-			printf("%d\n", arr[i]);
-			i++;
+			if (arr[i] < arr[j])
+			{
+				ft_swap(&arr[i], &arr[j]);
+				break;
+			}
+			j--;
 		}
 		i++;
 	}
+}
+
+void	ft_print_arr(int *arr, int arr_size)
+{
+	size_t	i;
+
+	i = 0;
+	printf("{ ");
+	while (i < arr_size)
+	{
+		if (i == arr_size - 1)
+			printf("%d", arr[i]);
+		else
+			printf("%d, ", arr[i]);
+		i++;
+	}
+	printf("}\n");
 }
 
 int main()
@@ -35,6 +58,8 @@ int main()
 	int arr_size = sizeof(arr) / sizeof(int);
 
 	printf("||---------------------------- ft_insertion_sort ----------------------------||\n\n");
+	// ft_swap(&arr[0], &arr[1]);
 	ft_insertion_sort(arr, arr_size);
+	ft_print_arr(arr, arr_size);
 	printf("\n||---------------------------------------------------------------------------||\n");
 }
