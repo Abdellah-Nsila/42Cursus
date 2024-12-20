d671aa7967575e4de9644a9aca8d59daeecba493
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-20 11:23:41 +0100

    Some Update

--- Milestone 2/push_swap/push_swap.h
+++ Milestone 2/push_swap/push_swap.h
@@ -9,9 +9,7 @@
 #include "./Libft/libft.h"
 
 
-// handle double linked list
-// commands for push - swap - rotate
-
+// Basic Struct for stack
 typedef struct s_node
 {
 	int n;
@@ -19,19 +17,25 @@ typedef struct s_node
 	struct s_node   *next;
 }	t_node ;
 
+// Some functions for debugging
 void	print_node(t_node *node);
 void	print_stack(t_node *stack);
+void	display_stacks(t_node *a, t_node *b, char *state, char *action);
+t_node	*genarate_stack();
+
+// Utils functions for double linked list
 t_node	*new(int n);
 t_node	*last(t_node *stack);
 void	push(t_node **stack, t_node *new);
 t_node	*pop(t_node **stack);
 
+// Push_Swap Cammands
 void	sa_sb(t_node **stack);
 void	ss(t_node **a, t_node **b);
 void	pa_pb(t_node **from_stack, t_node **to_stack);
 void	ra_rb_rra_rrb(t_node **stack, char type);
 void	rr_rrr(t_node **a, t_node **b, char type);
 
-// handle inputs
+//Todo Functions to handle inputs
 
 #endif 
