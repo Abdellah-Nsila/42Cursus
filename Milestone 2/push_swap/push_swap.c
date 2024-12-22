038fe9d067ea6acb337d6b826cc63a17a26e148d
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-22 16:11:59 +0100

    Some Update

--- Milestone 2/push_swap/push_swap.c
+++ Milestone 2/push_swap/push_swap.c
@@ -1,12 +1,39 @@
 #include "push_swap.h"
 
+// Example:
+
+//? 2 1 3 6 5 8  => pb pb rrb pb sa pa pa pa => 1 2 3 5 6 8 : 8 instructions 💪
+	//* pa_pb(&a, &b);
+	//* pa_pb(&a, &b);
+
+	//* ra_rb_rra_rrb(&b, 'R');
+	//* pa_pb(&a, &b);
+
+	//* sa_sb(&a);
+
+	//* pa_pb(&b, &a);
+	//* pa_pb(&b, &a);
+	//* pa_pb(&b, &a);
+
+
 int	main(int ac, char **av)
 {
 	t_stack	*a;
+	t_stack	*b;
 
+	b = NULL;
 	a = ft_init_stack(ac, av);
 	if (!a)
 		return (printf("Error\n"), 0);
-	display_stacks(a, NULL, "After", "Create");
+	display_stacks(a, b, "After", "Create");
 	
+	pa_pb(&a, &b);
+	pa_pb(&a, &b);
+	ra_rb_rra_rrb(&b, 'R');
+	pa_pb(&a, &b);
+	sa_sb(&a);
+	pa_pb(&b, &a);
+	pa_pb(&b, &a);
+	pa_pb(&b, &a);
+	display_stacks(a, b, "After", "Create");
 }
