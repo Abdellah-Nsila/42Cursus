e89f9be29af9781e189e0485e63c7066fd666648
Abdellah-Nsila <dawndawn074@gmail.com>
2024-12-30 11:04:47 +0100

    Remove Some alias

--- Milestone 2/push_swap/Libft/includes/get_next_line.h
+++ Milestone 2/push_swap/Libft/includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 15:29:38 by abnsila           #+#    #+#             */
-/*   Updated: 2024/12/30 09:58:12 by abnsila          ###   ########.fr       */
+/*   Updated: 2024/12/30 10:40:32 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ size_t	_ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*_ft_clean(char **static_var);
 
 char	*ft_append_buff(char **static_var, char *buff, ssize_t rb);
-char	*ft_get_remaining(char **static_var, char *end_line);
 char	*ft_get_line(char **static_var, char *end_line);
 char	*ft_handle_end_of_read(char **static_var, char *buff, ssize_t rb);
 
