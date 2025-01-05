404cc1f6fafad269bd87ec38e8162a88a4d2cc34
Abdellah-Nsila <dawndawn074@gmail.com>
2025-01-05 11:46:26 +0100

    Update header file

--- Milestone 2/pipex/utils.h
+++ Milestone 2/pipex/utils.h
@@ -6,12 +6,12 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 11:45:07 by abnsila           #+#    #+#             */
-/*   Updated: 2025/01/05 11:45:08 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/01/05 11:46:18 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef UTILS_H
+# define UTILS_H
 
 #include <unistd.h>
 #include <stdio.h>
