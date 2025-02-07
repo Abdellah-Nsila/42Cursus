d0a6f966e8f6a2719ad790e7dd1486019fb6920b
Abdellah-Nsila <dawndawn074@gmail.com>
2025-02-07 12:21:02 +0100

    Make range color shift by adding SHIFT_KEY event

--- Milestone 2/fract-ol/includes/fractol.h
+++ Milestone 2/fract-ol/includes/fractol.h
@@ -29,6 +29,7 @@
 # define MOVE_FACTOR 0.1
 # define INSIDE_COLOR 0x000000
 # define DEFAULT_ITERATIONS 30
+# define ESCAPE_LIMITE 4.0
 
 enum e_sets
 {
@@ -40,20 +41,27 @@ enum e_sets
 	Burningship_cube,
 };
 
-enum e_events
+enum e_events_hooks
 {
-	INCREASE_PRECISION = 1,
-	DECREASE_PRECISION = 3,
+	ON_MOUSEMOVE = 6,
+	ON_DESTROY = 17,
+};
+
+enum e_events_inputs
+{
+	INCREASE_BUTTON = 1,
+	DECREASE_BUTTON = 3,
 	ZOOM_IN = 4,
 	ZOOM_OUT = 5,
-	LEFT = 65361,
-	UP = 65362,
-	RIGHT = 65363,
-	DOWN = 65364,
-	ESCAPE = 65307,
-	PLUS = 65451,
-	MINUS = 65453,
-	t = 116,
+	LEFT_KEY = 65361,
+	UP_KEY = 65362,
+	RIGHT_KEY = 65363,
+	DOWN_KEY = 65364,
+	ESCAPE_KEY = 65307,
+	PLUS_KEY = 65451,
+	MINUS_KEY = 65453,
+	SHIFT_KEY = 65505,
+	SPACE_KEY = 32,
 };
 
 typedef struct s_img
@@ -89,7 +97,7 @@ typedef struct s_fractol
 	int		end_color;
 	double	fixed_c_re;
 	double	fixed_c_img;
-	t_bool	trick_mouse;
+	int		trick_mouse;
 }				t_fractol;
 
 typedef struct s_complex
@@ -106,11 +114,12 @@ void		ft_check_parse(t_fractol *fractol, int ac, char **av);
 void		ft_init_fractol(t_fractol *fractol);
 void		ft_init_image_buffer(t_fractol *fractol);
 void		ft_clean_fractol(t_fractol *fractol);
+int			ft_close(t_fractol *fractol);
 
 // Events Hook
-int			ft_mouse_hook(int button, int x, int y, void *param);
-int			ft_key_hook(int keycode, void *param);
-int			ft_mouse_move_hook(int x, int y, void *param);
+int			ft_mouse_hook(int button, int x, int y, t_fractol *fractol);
+int			ft_key_hook(int keycode, t_fractol *fractol);
+int			ft_mouse_move_hook(int x, int y, t_fractol *fractol);
 
 // 2D logical plane Utils
 void		ft_init_complex_plan(t_fractol *fractol);
@@ -123,7 +132,7 @@ void		ft_move_plan(t_fractol *fractol, double offset_x, double offset_y);
 void		ft_draw_fractal(t_fractol *fractol);
 void		ft_walk(t_fractol *fractol, int (*fractal_func)(t_fractol *, t_complex));
 
-// Sets
+// Fractal Sets Equations
 int			ft_mandelbrot_sq_set(t_fractol *fractol, t_complex c);
 int			ft_mandelbrot_cub_set(t_fractol *fractol, t_complex c);
 int			ft_julia_sq_set(t_fractol *fractol, t_complex c);
