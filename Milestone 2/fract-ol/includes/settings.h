528bd235cfa75302019e8af6d4f7bc3515968194
Abdellah-Nsila <dawndawn074@gmail.com>
2025-02-09 12:30:58 +0100

    Separate Header files

---
+++ Milestone 2/fract-ol/includes/settings.h
@@ -0,0 +1,53 @@
+#ifndef FT_SETTINGS_H
+# define FT_SETTINGS_H
+
+# define WIDTH 900
+# define HEIGHT 700
+# define CENTER_X -0.5
+# define CENTER_Y 0.0
+# define RANGE_X 3.5
+# define DEFAULT_ZOOM 1
+# define ZOOM_FACTOR 1.08
+# define MAX_ZOOM 508689079416445.0
+# define MIN_ZOOM 0.00457443136616103498
+# define PRECISION_FACTOR 5
+# define MAX_PRECISION 900
+# define MIN_PRECISION 1
+# define MOVE_FACTOR 0.2
+# define INSIDE_COLOR 0x000000
+# define DEFAULT_ITERATIONS 30
+# define ESCAPE_LIMITE 4.0
+# define PALETTE_SIZE 13
+
+enum e_sets
+{
+	Mandelbrot_square,
+	Mandelbrot_cube,
+	Julia_square,
+	Julia_cube,
+	Burningship_square,
+	Burningship_cube,
+};
+
+enum e_events_hooks
+{
+	ON_MOUSEMOVE = 6,
+	ON_DESTROY = 17,
+};
+
+enum e_events_inputs
+{
+	ZOOM_IN = 4,
+	ZOOM_OUT = 5,
+	INCREASE_BUTTON = 1,
+	DECREASE_BUTTON = 3,
+	LEFT_KEY = 65361,
+	UP_KEY = 65362,
+	RIGHT_KEY = 65363,
+	DOWN_KEY = 65364,
+	ESCAPE_KEY = 65307,
+	SHIFT_KEY = 65505,
+	SPACE_KEY = 32,
+};
+
+#endif
