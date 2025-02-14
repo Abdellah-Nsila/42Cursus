07870ddeb0cd22cde9f09df7f40d9b5c9f53c07b
Abdellah-Nsila <dawndawn074@gmail.com>
2025-02-14 16:29:25 +0100

    Understand thread creation process and join

--- Milestone 3/Philosophers/philo/src/philosophers.c
+++ Milestone 3/Philosophers/philo/src/philosophers.c
@@ -6,40 +6,31 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:48:15 by abnsila           #+#    #+#             */
-/*   Updated: 2025/02/14 08:45:22 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/02/14 16:26:41 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-// TODO return a num and get it
-void	*ft_random_num(void *arg)
-{
-	int	i;
-
-	i = (rand() % 6) + 1;
-	return (NULL);
-}
-
-void	*ft_routine(void *arg)
-{
-	t_philo *philo = (t_philo *)arg;
-	int	i = 0;
-
-	while (i < 1000000)
-	{	
-		pthread_mutex_lock(&philo->mutex);
-		philo->score++;
-		i++;
-		pthread_mutex_unlock(&philo->mutex);
-	}
-	return (NULL);
-}
-
 void	ft_init_philo(t_philo *philo)
 {
+	srand(time(NULL));
 	pthread_mutex_init(&philo->mutex, NULL);
 	philo->score = 0;
+}
+
+// TODO return a num and get it
+void	*ft_random_num()
+{
+	int	i;
+	int	*res;
+
+	res = malloc(sizeof(int));
+	i = (rand() % 6) + 1;
+	printf("Random Num in: %d\n", i);
+	*res = i;
+	
+	return ((void *)res);
 }
 
 int	main()
@@ -47,26 +38,76 @@ int	main()
 	t_philo		philo;
 	pthread_t	threads[THREADS];
 	int			i = 0;
+	int			sum = 0;
+	int			*res;
 
 	ft_init_philo(&philo);
-	// Create threads
+	
 	while (i < THREADS)
 	{	
-		if (pthread_create(&threads[i], NULL, &ft_routine, &philo) != 0)
+		if (pthread_create(&threads[i], NULL, &ft_random_num, &philo) != 0)
 			return (EXIT_FAILURE);
-		printf("Thread Started Execution\n");
 		i++;
 	}
-	// Join thrades
 	i = 0;
 	while (i < THREADS)
 	{
-		if (pthread_join(threads[i], NULL) != 0)
+		if (pthread_join(threads[i], (void **)&res) != 0)
 			return (EXIT_FAILURE);
-		printf("Thread Finished Execution\n");
+		sum += *res;
 		i++;
 	}
-	printf("Score: %d\n", philo.score);
+	printf("\nSum of Random Num: %d\n", sum);
 	pthread_mutex_destroy(&philo.mutex);
 	return (0);
 }
+
+// void	*ft_routine(void *arg)
+// {
+// 	t_philo *philo = (t_philo *)arg;
+// 	int	i = 0;
+
+// 	while (i < 1000000)
+// 	{	
+// 		pthread_mutex_lock(&philo->mutex);
+// 		philo->score++;
+// 		i++;
+// 		pthread_mutex_unlock(&philo->mutex);
+// 	}
+// 	return (NULL);
+// }
+
+// void	ft_init_philo(t_philo *philo)
+// {
+// 	pthread_mutex_init(&philo->mutex, NULL);
+// 	philo->score = 0;
+// }
+
+// int	main()
+// {
+// 	t_philo		philo;
+// 	pthread_t	threads[THREADS];
+// 	int			i = 0;
+
+// 	ft_init_philo(&philo);
+// 	// Create threads
+// 	while (i < THREADS)
+// 	{	
+// 		if (pthread_create(&threads[i], NULL, &ft_routine, &philo) != 0)
+// 			return (EXIT_FAILURE);
+// 		printf("Thread Started Execution\n");
+// 		i++;
+// 	}
+// 	// Join thrades
+// 	i = 0;
+// 	while (i < THREADS)
+// 	{
+// 		if (pthread_join(threads[i], NULL) != 0)
+// 			return (EXIT_FAILURE);
+// 		printf("Thread Finished Execution\n");
+// 		i++;
+// 	}
+// 	printf("Score: %d\n", philo.score);
+// 	pthread_mutex_destroy(&philo.mutex);
+// 	return (0);
+// }
